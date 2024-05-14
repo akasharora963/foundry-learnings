5dcd66005ee00f4c004b5519980faca0332e270f
Akash Arora <akasharora963@gmail.com>
2024-05-14 18:57:08 +0530

    test with counter contract

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
@@ -9,16 +9,27 @@ contract CounterTest is Test {
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
     function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        counter.inc();
+        assertEq(counter.count(), 1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_Decrement() public {
+        assertEq(counter.count(), 0);
+        counter.inc();
+        assertEq(counter.count(), 1);
+        counter.dec();
+        assertEq(counter.count(), 0);
+    }
+
+    function testFailDec() public {
+        counter.dec();
+    }
+
+    function testDecUnderFlow() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
     }
 }
