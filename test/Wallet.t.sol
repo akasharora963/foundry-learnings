a41053edd5e9e31155ab7b72ca98405195a1f766
Akash Arora <akasharora963@gmail.com>
2024-05-14 21:29:50 +0530

    add: auth test for wallet

---
+++ test/Wallet.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console, stdError} from "forge-std/Test.sol";
+import {Wallet} from "../src/Wallet.sol";
+
+contract AuthTest is Test {
+    Wallet public wallet;
+
+    function setUp() public {
+        wallet = new Wallet();
+    }
+
+    function testSetOwner() public {
+        wallet.setOwner(address(1));
+        assertEq(wallet.owner(), address(1));
+    }
+
+    function testFailNotOwner() public {
+        vm.prank(address(1));
+        wallet.setOwner(address(1));
+    }
+
+    function testFailSetOwnerAgain() public {
+        //msg.sender = address(this)
+        wallet.setOwner(address(1));
+
+        vm.startPrank(address(1));
+        //msg.sender = address(1)
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+
+        vm.stopPrank();
+        //msg.sender = address(this)
+        wallet.setOwner(address(1));
+    }
+}
